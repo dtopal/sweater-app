cc1a49c66181972c125051c40abda4b0a376b5d0
dtopal <11358089+dtopal@users.noreply.github.com>
2020-03-08 17:36:40 +0900

    Correct syntax

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, rers) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
